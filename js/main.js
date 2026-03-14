4d247656c182d990bd9d31294aaa5e97d4112d8d
Mustafa Atta <110784859+mustafaatta20012@users.noreply.github.com>
2026-03-14 23:36:39 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,87 +1,3 @@
-(function ($) {
-    "use strict";
-
-    // Spinner
-    var spinner = function () {
-        setTimeout(function () {
-            if ($('#spinner').length > 0) {
-                $('#spinner').removeClass('show');
-            }
-        }, 1);
-    };
-    spinner(0);
-    
-    
-    // Initiate the wowjs
-    new WOW().init();
-
-
-    // Sticky Navbar
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 45) {
-            $('.navbar').addClass('sticky-top shadow-sm');
-        } else {
-            $('.navbar').removeClass('sticky-top shadow-sm');
-        }
-    });
-
-
-    // testimonial carousel
-    $(".testimonial-carousel").owlCarousel({
-        autoplay: true,
-        smartSpeed: 1500,
-        center: false,
-        dots: true,
-        loop: true,
-        margin: 25,
-        nav : true,
-        navText : [
-            '<i class="fa fa-angle-right"></i>',
-            '<i class="fa fa-angle-left"></i>'
-        ],
-        responsiveClass: true,
-        responsive: {
-            0:{
-                items:1
-            },
-            576:{
-                items:1
-            },
-            768:{
-                items:1
-            },
-            992:{
-                items:2
-            },
-            1200:{
-                items:2
-            }
-        }
-    });
-
-
-    // Facts counter
-    $('[data-toggle="counter-up"]').counterUp({
-        delay: 5,
-        time: 2000
-    });
-
-
-   // Back to top button
-   $(window).scroll(function () {
-    if ($(this).scrollTop() > 300) {
-        $('.back-to-top').fadeIn('slow');
-    } else {
-        $('.back-to-top').fadeOut('slow');
-    }
-    });
-    $('.back-to-top').click(function () {
-        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
-        return false;
-    });
-
-
-})(jQuery);
 /* ==========================================
    SPINNER LOADER
    Hide loading spinner after full page load
@@ -132,30 +48,29 @@ window.addEventListener("scroll", function () {
 });
 // show number phone 
 
-    function animatePhoneNumbers() {
+//     function animatePhoneNumbers() {
 
-  const numbers = document.querySelectorAll(".phone-number");
+//   const numbers = document.querySelectorAll(".phone-number");
 
-  numbers.forEach(el => {
+//   numbers.forEach(el => {
 
-    const finalNumber = el.dataset.number;
-    let current = "";
-    let i = 0;
+//     const finalNumber = el.dataset.number;
+//     let current = "";
+//     let i = 0;
 
-    function typeNumber() {
-      if (i < finalNumber.length) {
-        current += finalNumber[i];
-        el.textContent = current;
-        i++;
-        setTimeout(typeNumber, 120);
-      }
-    }
+//     function typeNumber() {
+//       if (i < finalNumber.length) {
+//         current += finalNumber[i];
+//         el.textContent = current;
+//         i++;
+//         setTimeout(typeNumber, 120);
+//       }
+//     }
 
-    typeNumber();
+//     typeNumber();
 
-  });
+//   });
 
-}
+// }
 
-window.addEventListener("load", animatePhoneNumbers);
-
+// window.addEventListener("load", animatePhoneNumbers);
